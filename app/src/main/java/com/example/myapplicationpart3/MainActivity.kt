79da053a48742268669f1c4e94556e4c8c44e1b3
andre <andrey.kladov.04@mail.ru>
2025-02-01 01:00:03 +0300

    0.1

---
+++ app/src/main/java/com/example/myapplicationpart3/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myapplicationpart3
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplicationpart3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(activityMainBinding.root)
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i("Метод жизненного цикла", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Метод жизненного цикла", "onStop")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Метод жизненного цикла", "onDestroy")
+    }
+}
